b9b58134544af6978a4b8e139016626cc7ab53f5
EGBoldyr <evgenboldyr@gmail.com>
2018-04-22 17:56:15 +0300

    LAN

---
+++ Pro005_Examples/src/main/java/lan/lesson08/LanEx04.java
@@ -0,0 +1,30 @@
+package lan.lesson08;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+
+public class LanEx04 {
+
+    public static void main(String[] args) throws IOException {
+        SocketChannel channel =
+                SocketChannel.open(
+                        new InetSocketAddress("127.0.0.1", 20000));
+
+        ByteBuffer buffer = ByteBuffer.allocate(128);
+        String message;
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            System.out.print("Сообщение для отправки : ");
+            message = scan.nextLine();
+            buffer.put(message.getBytes());
+            buffer.flip();
+            channel.write(buffer);
+            buffer.clear();
+
+        } while (!message.equals("exit"));
+    }
+}
